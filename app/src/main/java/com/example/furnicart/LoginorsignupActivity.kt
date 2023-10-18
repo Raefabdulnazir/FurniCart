6155408be8ea10772ec4a40604616e7b21eb9c62
Raefabdulnazir <raefnazir@gmail.com>
2023-10-18 23:33:05 +0400

    created a signup page

---
+++ app/src/main/java/com/example/furnicart/LoginorsignupActivity.kt
@@ -0,0 +1,11 @@
+package com.example.furnicart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginorsignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loginorsignup)
+    }
+}
