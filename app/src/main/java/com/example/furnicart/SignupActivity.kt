a2e132867c53bb690fc7db6bddc8d41b33d65e91
Raef Nazir <raefnazir@gmail.com>
2023-10-19 11:30:20 +0400

    added page switching(login to signup and viceversa)

--- app/src/main/java/com/example/furnicart/SignupActivity.kt
+++ app/src/main/java/com/example/furnicart/SignupActivity.kt
@@ -1,11 +1,21 @@
 package com.example.furnicart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val alreadyHaveAnAccountTextView = findViewById<TextView>(R.id.aldreadyHaveAnAccountTextView)
+        alreadyHaveAnAccountTextView.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
